5e559613436b226c4be8cf4407dea5cc3c35a675
Juan Ruiz <jruiz@everydayhealthgroup.com>
2023-07-10 11:56:19 -0400

    add find in list

---
+++ find-in-list.js
@@ -0,0 +1,13 @@
+/*Input:
+Initially you get a list of strings
+
+'earth', 'jupiter', microscope',...
+
+then you get a word and have to find if is in initial list
+
+
+Output: Boolean
+*/
+
+function isInList(word) {}
+
